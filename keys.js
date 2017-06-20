5a54cb02fe745f9720bc5e1250cbf40b74ab8da1
Tin Nguyen <Tinman@Tins-MacBook-Pro.local>
2017-06-19 21:30:29 -0500

    Updating with all required functions

--- keys.js
+++ keys.js
@@ -3,6 +3,12 @@ console.log('this is loaded');
 exports.twitterKeys = {
   consumer_key: 'Zrg6ce95Wgh4wBPBPF9gpgPmc',
   consumer_secret: 'emqs7MQV8aA5TgZ8gtGUwyFITFMj3dmM7aL9PTmQoe9KGzmsET',
+  access_token: '875137985595023360-UXAI0S9JCPI3Cu0152hAcPcIhfeu2dY',
   access_token_key: '875137985595023360-UXAI0S9JCPI3Cu0152hAcPcIhfeu2dY',
   access_token_secret: 'yjxsmVqK3UMeKN7orIShmezqUK99LrUhhZgbjfdQL5KOQ',
+}
+
+exports.spotifyKeys = {
+  	id: 'a802f4e8b96c43c381604b5d6f9f03ff',
+  	secret: '59bc4f88ff484794bdd7e3a1b0fa2421'
 }
--- liri.js
+++ liri.js
@@ -1,14 +1,23 @@
-// Client ID: a802f4e8b96c43c381604b5d6f9f03ff
-// Client Secret: 59bc4f88ff484794bdd7e3a1b0fa2421
 
 var fs = require("fs");
 var request = require('request');
 var Spotify = require('node-spotify-api');
+var Twit = require('twit');
+var Twitter = require('twitter');
+var keys = require('./keys')
 
-var spotify = new Spotify({
-  id: 'a802f4e8b96c43c381604b5d6f9f03ff',
-  secret: '59bc4f88ff484794bdd7e3a1b0fa2421'
-});
+var spotify = new Spotify(
+	keys.spotifyKeys
+);
+
+var T = new Twit(
+	keys.twitterKeys	
+)
+
+var client = new Twitter(
+	keys.twitterKeys
+);
+ 
  
 whatYouWant(process.argv[2]); 
 
@@ -29,6 +38,34 @@ function whatYouWant(x,y){
 	}
 }
 
+function tweetThis(){
+	var params = {screen_name: 'realDonaldTrump'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response) {
+	  	if (!error) {
+	  		var result = "";
+		  	for(var i =0; i<10;i++){
+				result +=tweets[i].created_at +"\n"+
+		  			tweets[i].text +"\n"+
+		  			tweets[i].user.name +"\n"
+		  			+"========================="+"\n"
+			}
+			console.log(result);
+			logThis(result);
+	  	}
+	});
+
+	// T.get('search/tweets', { q: 'banana since:2011-07-11', count: 10 }, function(err, data, response) {
+	// 	for(var i =0; i<data.statuses.length;i++){
+	// 		console.log(
+	//   			data.statuses[i].created_at +"\n"+
+	//   			data.statuses[i].text +"\n"+
+	//   			data.statuses[i].user.name +"\n"
+	//   			+"========================="
+	//   		)
+	// 	}
+	// })
+}
+
 function doThis(){
 	fs.readFile("random.txt", "utf8", function(err, data) {
 
@@ -36,7 +73,6 @@ function doThis(){
 	    	return console.log(err);
 	  	}
 	  	var dataArr = data.split(",");
-	  	//dataArr.map(function(x){console.log(x)});
 	  	whatYouWant(dataArr[0], dataArr[1]);
 	});
 }
@@ -55,16 +91,15 @@ function spotifyThis(y){
 		songName="The Sign Ace of Base"
 	}
 	spotify.search({ type: 'track', query: songName, limit: 1 }, function(err, data) {
-		console.log(process.argv.slice(3).join(" "));
 		if (err) {
 			return console.log('Error occurred: ' + err);
 		}
-		console.log(
-	    	"Artist(s): " + data.tracks.items[0].artists[0].name  +"\n"+ 
+		var result = "Artist(s): " + data.tracks.items[0].artists[0].name  +"\n"+ 
 	    	"Song Title : " + data.tracks.items[0].name  +"\n"+ 
 	    	"Spotify Preview Link : " + data.tracks.items[0].preview_url  +"\n"+ 
 	    	"Album : " + data.tracks.items[0].album.name  +"\n"
-	    );
+		console.log(result);
+		logThis(result);
 	});
 }
 
@@ -83,20 +118,25 @@ function movieThis(y) {
 	}
 
 	var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
-	console.log(queryUrl);
 	request(queryUrl, function(error, response, body) {
 
-	  if (!error && response.statusCode === 200) {
-	    console.log(
-	    	"Title: " + JSON.parse(body).Title  +"\n"+ 
-	    	"Year: " + JSON.parse(body).Year  +"\n"+ 
-	    	"IMDB Rating: " + JSON.parse(body).imdbRating  +"\n"+ 
-	    	"Country: " + JSON.parse(body).Country  +"\n"+ 
-	    	"Language: " + JSON.parse(body).Language  +"\n"+ 
-	    	"Plot: " + JSON.parse(body).Plot  +"\n"+ 
-	    	"Actors: " + JSON.parse(body).Actors  +"\n"
-	    	);
-	  }
+	  	if (!error && response.statusCode === 200) {
+	  		var result = "Title: " + JSON.parse(body).Title  +"\n"+ 
+		    	"Year: " + JSON.parse(body).Year  +"\n"+ 
+		    	"IMDB Rating: " + JSON.parse(body).imdbRating  +"\n"+ 
+		    	"Country: " + JSON.parse(body).Country  +"\n"+ 
+		    	"Language: " + JSON.parse(body).Language  +"\n"+ 
+		    	"Plot: " + JSON.parse(body).Plot  +"\n"+ 
+		    	"Actors: " + JSON.parse(body).Actors  +"\n"
+		    console.log(result);
+		    logThis(result);
+	  	}
+	});
+}
+
+function logThis(z){
+	fs.appendFile('log.txt', z+"\n", (err) => {
+	  	if (err) throw err;
 	});
 }
 
